b5e895303894733e31600916be894cb4d8f43f15
thomas clamon <thomas.clamon@gmail.com>
2022-12-15 16:21:30 +0100

    Add age

--- src/main/java/com/egsi/ecoleegsi/ressources/EtudiantRessources.java
+++ src/main/java/com/egsi/ecoleegsi/ressources/EtudiantRessources.java
@@ -1,11 +1,13 @@
 package com.egsi.ecoleegsi.ressources;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.egsi.ecoleegsi.dto.EtudiantDto;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("Etudiant")
 public class EtudiantRessources {
-
+    @PostMapping("age")
+    public Integer afficherAge(@RequestBody EtudiantDto dto){
+        return dto.calculerAge();
+    }
 }
