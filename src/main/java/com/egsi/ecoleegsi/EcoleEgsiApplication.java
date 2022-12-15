0326f03d4ffca307c9bc418c409479076febf4df
thomas clamon <thomas.clamon@gmail.com>
2022-12-15 11:48:01 +0100

    feat(java) : init project

---
+++ src/main/java/com/egsi/ecoleegsi/EcoleEgsiApplication.java
@@ -0,0 +1,13 @@
+package com.egsi.ecoleegsi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoleEgsiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcoleEgsiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/egsi/ecoleegsi/EcoleEgsiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.egsi.ecoleegsi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoleEgsiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
