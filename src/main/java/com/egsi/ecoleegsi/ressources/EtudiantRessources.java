930ce45f2538b65e0eaa5b27b244b94d1fe5707e
ryan <chambre.ryan@orange.fr>
2022-12-15 16:40:04 +0100

    feat(api): add new class MatiereDTO + new endpoint afficher intitulé

--- src/main/java/com/egsi/ecoleegsi/ressources/EtudiantRessources.java
+++ src/main/java/com/egsi/ecoleegsi/ressources/EtudiantRessources.java
@@ -1,6 +1,8 @@
 package com.egsi.ecoleegsi.ressources;
 
 import com.egsi.ecoleegsi.dto.EtudiantDto;
+import com.egsi.ecoleegsi.dto.Matiere;
+import com.egsi.ecoleegsi.dto.MatiereDTO;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -9,5 +11,10 @@ public class EtudiantRessources {
     @PostMapping("age")
     public Integer afficherAge(@RequestBody EtudiantDto dto){
         return dto.calculerAge();
+    }
+
+    @PostMapping("matiere")
+    public String afficherIntituler(@RequestBody MatiereDTO dto){
+        return dto.getIntitule();
     }
 }
